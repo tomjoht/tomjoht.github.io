30ede1c4369f5000772f02dc5e8aa9c92df8c6e4
Tom Johnson <tomjoht@gmail.com>
2020-10-22 12:53:28 -0700

    new madcap ads

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
-myvenv
-aws s3 cp ~/projects/idratherbewriting/images/$1 s3://idbwmedia.com/images/ --profile wasabi
+source /Users/tomjoht/projects/myvenv/bin/activate
+aws s3 cp ~/projects/idratherbewriting.io/images/$1 s3://idbwmedia.com/images/ --profile wasabi
 # to run, store image in project's image folder
 # then type this: . upload.sh image.png
 echo '<a href=""><img src="https://s3.us-west-1.wasabisys.com/idbwmedia.com/images/'$1
 echo '" alt="" /></a>'
-deactivate
+deactivate /Users/tomjoht/projects/myvenv/bin/activate
