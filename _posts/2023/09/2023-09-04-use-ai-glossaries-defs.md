452d5515cea5f137c58d49a9ae1f3b5bf4d64c3c
Tom Johnson <tomjoht@gmail.com>
2023-09-04 15:10:22 -0700

    add blog post pointer about AI glossaries topic added to course

---
+++ _posts/2023/09/2023-09-04-use-ai-glossaries-defs.md
@@ -0,0 +1,18 @@
+---
+title: "New API doc course topic: Using AI for glossary definitions"
+permalink: /blog/use-ai-glossaries-defs
+date: 2023-09-04
+categories:
+- ai
+- api-doc
+- api-doc-site-updates
+- writing
+keywords: 
+rebrandly: https://idbwrtng.com/use-ai-glossaries-defs
+description: "Compiling and maintaining glossaries can be time consuming. Can AI tools help with glossaries? Just as AIs are good at creating summaries, providing definitions might be one of AI's strengths. A definition is just a short summary of a concept. AI tools are also good at identifying potentially unfamiliar, jargon-filled terms and then listing concise definitions for them."
+---
+
+See [Using AI for glossary definitions](/learnapidoc/docapis_ai_glossary_definitions.html) for details.
+
+{% include ads.html %}
+
--- pdf_build_docapis_all.sh
+++ pdf_build_docapis_all.sh
@@ -14,7 +14,7 @@ pdftk $LOCATION/docapis_all_chapters.pdf cat 113-173 output $LOCATION/chapter3_d
 pdftk $LOCATION/docapis_all_chapters.pdf cat 174-265 output $LOCATION/chapter4_openapi_spec_and_generated_reference_docs.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 266-336 output $LOCATION/chapter5_step_by_step_openapi_code_tutorial.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 337-352 output $LOCATION/chapter6_testing_api_docs.pdf
-pdftk $LOCATION/docapis_all_chapters.pdf cat 353-425 output $LOCATION/chapter7_conceptual_topics_in_api_docs.pdf
+pdftk $LOCATION/docapis_all_chapters.pdf cat 353-426 output $LOCATION/chapter7_conceptual_topics_in_api_docs.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 427-495 output $LOCATION/chapter8_code_tutorials.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 496-524 output $LOCATION/chapter9_the_writing_process.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 525-588 output $LOCATION/chapter10_publishing_api_docs.pdf
@@ -23,9 +23,9 @@ pdftk $LOCATION/docapis_all_chapters.pdf cat 655-686 output $LOCATION/chapter12_
 pdftk $LOCATION/docapis_all_chapters.pdf cat 687-722 output $LOCATION/chapter13_native_library_apis.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 723-795 output $LOCATION/chapter14_processes_and_methodology.pdf
 pdftk $LOCATION/docapis_all_chapters.pdf cat 796-821 output $LOCATION/chapter15_metrics_and_measurement.pdf
-pdftk $LOCATION/docapis_all_chapters.pdf cat 822-887 output $LOCATION/chapter16_ai_and_api_docs.pdf
-pdftk $LOCATION/docapis_all_chapters.pdf cat 888-917 output $LOCATION/chapter17_glossary.pdf
-pdftk $LOCATION/docapis_all_chapters.pdf cat 918-981 output $LOCATION/chapter18_additional_resources.pdf
+pdftk $LOCATION/docapis_all_chapters.pdf cat 822-892 output $LOCATION/chapter16_ai_and_api_docs.pdf
+pdftk $LOCATION/docapis_all_chapters.pdf cat 893-930 output $LOCATION/chapter17_glossary.pdf
+pdftk $LOCATION/docapis_all_chapters.pdf cat 931-994 output $LOCATION/chapter18_additional_resources.pdf
 
 echo "Now zipping everything up"
 zip pdf/docapis.zip pdf/readme.txt pdf/docapis_all_chapters.pdf pdf/frontmatter.pdf pdf/chapter1_introduction_to_rest_apis.pdf pdf/chapter2_using_an_api_like_a_developer.pdf pdf/chapter3_documenting_api_endpoints.pdf pdf/chapter4_openapi_spec_and_generated_reference_docs.pdf pdf/chapter5_step_by_step_openapi_code_tutorial.pdf pdf/chapter6_testing_api_docs.pdf pdf/chapter7_conceptual_topics_in_api_docs.pdf pdf/chapter8_code_tutorials.pdf pdf/chapter9_the_writing_process.pdf pdf/chapter10_publishing_api_docs.pdf pdf/chapter11_publishing_tools.pdf pdf/chapter12_thriving_in_the_api_doc_space.pdf pdf/chapter13_native_library_apis.pdf pdf/chapter14_processes_and_methodology.pdf pdf/chapter15_metrics_and_measurement.pdf pdf/chapter16_ai_and_api_docs.pdf pdf/chapter17_glossary.pdf pdf/chapter18_additional_resources.pdf
