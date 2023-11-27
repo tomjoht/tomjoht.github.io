207127f4aaf4481967837093c5b24c34ae237558
Tom Johnson <tomjoht@gmail.com>
2023-11-27 07:33:27 -0800

    first post

--- pdf_build_smartphones.sh
+++ pdf_build_smartphones.sh
@@ -3,3 +3,4 @@ bundle exec jekyll build --config _config_pdf_smartphones.yml --limit_posts 1;
 echo "the PDF ...";
 prince --javascript --input-list=_site/prince-list-all.txt --no-warn-css-unsupported -o pdf/journey_away_from_smartphones.pdf;
 echo "done building smartphones";
+echo "now run ./pdf_upload_smartphones.sh to upload it"
