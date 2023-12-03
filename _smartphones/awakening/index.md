20310620a5b11e4dcbd5a056160931b783f4ae45
Tom Johnson <tomjoht@gmail.com>
2023-12-02 20:11:02 -0800

    improve summaries for the focus book

--- _smartphones/awakening/index.md
+++ _smartphones/awakening/index.md
@@ -10,8 +10,9 @@ redirect_from:
 - /smartphones/overview.html
 ---
 
+This series started with the realization that smartphones were fragmenting my attention span, so I abandoned my smartphone for six weeks. During that time, I rediscovered books and reading. I did eventually return to my smartphone because disconnecting from it also meant disconnecting from family, but I continued reading and making more space in my life to consume more of the content offline, rather than the social media feeds on my smartphone. 
 
-This is an intro to the entire series...
+The greatest outcome of my basic phone experiment was to rekindle interest and enjoyment in reading books. Long-form reading was an activity that I’d sort of forgotten, and even though I reverted to my smartphone, the experiment taught me to love reading again. 
 
 {% include coffeeshopbook-smartphones.html %}
 
--- pdf_build_smartphones.sh
+++ pdf_build_smartphones.sh
@@ -10,10 +10,10 @@ LOCATION="/Users/tomjoht/projects/idratherbewriting/pdf"
 
 echo "now splitting the comprehensive PDF up into individual chapters"
 pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 1-3 output $LOCATION/frontmatter.pdf
-pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 4-61 output $LOCATION/chapter1-awakening.pdf
-pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 62-113 output $LOCATION/chapter2-wayfinding.pdf
-pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 114-201 output $LOCATION/chapter3-flow.pdf
-pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 202-224 output $LOCATION/chapter4-pushandpull.pdf
+pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 4-62 output $LOCATION/chapter1-awakening.pdf
+pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 63-114 output $LOCATION/chapter2-wayfinding.pdf
+pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 115-202 output $LOCATION/chapter3-flow.pdf
+pdftk $LOCATION/journey_away_from_smartphones_all_chapters.pdf cat 203-227 output $LOCATION/chapter4-pushandpull.pdf
 
 echo "Now zipping everything up"
 zip pdf/journey_away_from_smartphones.zip pdf/readme_smartphones.txt pdf/frontmatter.pdf pdf/chapter1-awakening.pdf pdf/chapter2-wayfinding.pdf pdf/chapter3-flow.pdf pdf/chapter4-pushandpull.pdf pdf/journey_away_from_smartphones_all_chapters.pdf
